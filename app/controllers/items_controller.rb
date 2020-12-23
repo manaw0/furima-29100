f8b00d98fe4f402b86c54a6bb33a8735d771189b
manaw0 <71313417+manaw0@users.noreply.github.com>
2020-12-23 14:13:40 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
-  def index
+  def index 
+    @items = Item.all
+  end
+
+  def new 
+    @Item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_days_id).merge(user_id: current_user.id)
   end
 end
