03fbbe2b2b588ee5f5cb02a694c7b79e8ce5f544
manaw0 <71313417+manaw0@users.noreply.github.com>
2020-12-23 13:59:48 +0900

    バリデーション、ルーティング、アソシエーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :price
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_days_id
+  end
+end
