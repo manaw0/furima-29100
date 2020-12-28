4b2d78c04fc000bf6ab7baa2a3be46ae0c26d765
manaw0 <71313417+manaw0@users.noreply.github.com>
2020-12-28 18:17:30 +0900

    ビューファイル修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index 
     @items = Item.all
   end
 
   def new 
-    @Item = Item.new
+    @item = Item.new
   end
 
   def create
-    Item.create(item_params)
-    if @message.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,18 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :content
-    validates :price
-    validates :category_id
-    validates :status_id
-    validates :shipping_fee_id
-    validates :prefecture_id
-    validates :shipping_day_id
     validates :image
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_fee_id
+      validates :prefecture_id
+      validates :shipping_day_id
+    end
+
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9,999,999 }
+    end
   end
 end
