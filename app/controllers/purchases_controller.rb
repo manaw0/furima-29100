a2a175c4f4f5a7571eb8c43eeb7998445f4f66e9
manaw0 <71313417+manaw0@users.noreply.github.com>
2021-01-12 13:49:13 +0900

    Formオブジェクト実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,28 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @oder_form = OderForm.new
+  end
+
+  def create
+    @oder_form = OderForm.new(purchase_params)
+    if @oder_form.valid?
+      @oder_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:oder_form).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
