b75f4952b76a991c3969c9cd11fabf67df375f86
manaw0 <71313417+manaw0@users.noreply.github.com>
2021-01-13 09:47:12 +0900

    テストコード実装

--- app/models/oder_form.rb
+++ app/models/oder_form.rb
@@ -5,7 +5,7 @@ class OderForm
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
