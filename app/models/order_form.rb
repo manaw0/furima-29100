075d8a6ad9ada03afef6a8f90a941b4a83bd6897
manaw0 <71313417+manaw0@users.noreply.github.com>
2021-01-17 17:41:06 +0900

    購入機能

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,7 +1,7 @@
 class OrderForm
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :purchase_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :purchase_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -9,6 +9,7 @@ class OrderForm
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
+    validates :token
   end
 
   def save
